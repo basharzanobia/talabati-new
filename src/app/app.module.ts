3e0e4e4e2e1f740150cb0b64e739b1541ede4e53
supportwebz <support_web@zanobia.me>
2022-04-13 14:53:11 +0300

    Add app initializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppInitializer } from './app-initializer';
+import { AppConsts } from '../shared/AppConsts';
+import { API_BASE_URL } from '../shared/service-proxies/service-proxies';
+import { ServiceProxyModule } from '../shared/service-proxies/service-proxy.module';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ServiceProxyModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (appInitializer: AppInitializer) => appInitializer.init(),
+      deps: [AppInitializer],
+      multi: true,
+    },
+    { provide: API_BASE_URL, useFactory: () => AppConsts.remoteServiceBaseUrl },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/shared/AppConsts.ts
@@ -0,0 +1,22 @@
+﻿export class AppConsts {
+
+    static remoteServiceBaseUrl: string;
+    static appBaseUrl: string;
+    static appBaseHref: string; // returns angular's base-href parameter value if used during the publish
+    static logoAssetBaseUrl: string;
+
+    static localeMappings: any = [];
+
+    static readonly userManagement = {
+        defaultAdminUserName: 'admin'
+    };
+
+    static readonly localization = {
+        defaultLocalizationSourceName: 'talabati'
+    };
+
+    static readonly authorization = {
+        encryptedAuthTokenName: 'enc_auth_token'
+    };
+}
+
