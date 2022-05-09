2963260406e0c46fbb4caff1f7e23930ede17a9a
supportwebz <support_web@zanobia.me>
2022-05-09 16:31:43 +0300

    Fetch vendor subcategories

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { AppConsts } from 'src/shared/AppConsts';
+import { VendorapiServiceProxy, VendorSubCategory } from 'src/shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  catId:number;
+  subcats$: Observable<VendorSubCategory[]>;
 
-  constructor() {}
+  AppConsts = AppConsts;
+
+  constructor(private route: ActivatedRoute,
+    private _vendorService: VendorapiServiceProxy) {}
+  
+  ngOnInit(): void {
+    this.subcats$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.catId = Number(params.get('catId'));
+        return this._vendorService.subcategories(this.catId);
+      })
+    );
+  }
 
 }
