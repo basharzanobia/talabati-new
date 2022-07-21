d3e115587336eb9f3a9056b058ca24824f473854
supportwebz <support_web@zanobia.me>
2022-07-21 15:41:10 +0300

    implement buy me

--- src/app/buy-for-me/buy-for-me.page.ts
+++ src/app/buy-for-me/buy-for-me.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import * as moment from 'moment';
+import { BuyMe, BuyrequestServiceProxy } from 'src/shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-buy-for-me',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyForMePage implements OnInit {
 
-  constructor() { }
+  reqModel: BuyMe = new BuyMe();
+
+  constructor(
+    private _service: BuyrequestServiceProxy,
+    private _router: Router,
+    public alertController: AlertController) { }
 
   ngOnInit() {
+
+  }
+
+  save() {
+    this.reqModel.recieverDate = moment(this.reqModel.recieverDate);
+    this.reqModel.senderDate = moment(this.reqModel.senderDate);
+
+    this._service.addrequest(this.reqModel).subscribe(
+    (res) => {
+        this._router.navigate(['/tabs/tab2']);
+    },
+    async (error) => {
+      // Unexpected result!
+      await this.presentAlert('فشل', 'حدث خطأ حاول مرة أخرى', null);
+      console.log('error ', error);
+    });
+  }
+
+  async presentAlert(header: string, msg: string, subHeader: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'app-alert',
+      header: header,
+      subHeader: subHeader,
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
   }
 
 }
