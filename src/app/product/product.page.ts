4a2ee32017328a0ea16180d5ba676a88f0a309cc
supportweb4Z <web4.zanoubia@gmail.com>
2022-09-16 11:34:51 +0200

    createWishList

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppConsts } from 'src/shared/AppConsts';
 import { CartStoreService } from 'src/shared/cart/cart-store.service';
+import { AppSessionService } from 'src/shared/session/app-session.service';
+import { AlertController } from '@ionic/angular';  
+
 import {
   Product,
-  ProductapiServiceProxy
+  ProductapiServiceProxy,
+  WishlistapiServiceProxy,
+  Wishlist,
 } 
 from 'src/shared/service-proxies/service-proxies';
 
@@ -20,9 +25,13 @@ export class ProductPage implements OnInit {
   AppConsts = AppConsts;
 
   constructor(
+    private _session: AppSessionService,
     private route: ActivatedRoute,
     public cart: CartStoreService,
-    private _productsService: ProductapiServiceProxy) { }
+    private _productsService: ProductapiServiceProxy,
+    private _wishlistService: WishlistapiServiceProxy,
+    public alertCtrl: AlertController
+    ) { }
 
   ngOnInit() {
     this.productId =  Number(this.route.snapshot.paramMap.get('productId'));
@@ -40,5 +49,34 @@ export class ProductPage implements OnInit {
     });
     
   }
+  async  showAlert() {  
+    const alert = await this.alertCtrl.create({  
+      header: 'المفضلة',  
+      subHeader: '',  
+      message: 'تمت إضافة المنتج إلى المفضلة بنجاح!',  
+      buttons: ['تم']  
+    });  
+    await alert.present();  
+    const result = await alert.onDidDismiss();  
+    console.log(result);  
+  }  
+  createWishList(){
+    const whishlist = new Wishlist();
+    whishlist.init({
+      productId: this.productId,
+      userId:this._session.userId,
+    });
+   
+    this._wishlistService.createwish(whishlist).subscribe(
+      (res) => {
+        this.showAlert();   
+      },
+      async (error) => {
+        // Unexpected result!
+        // await this.presentAlert('فشل', 'حدث خطأ حاول مرة أخرى', null);
+        console.log('error ', error);
+      });
+  }
+  }
 
-}
+
