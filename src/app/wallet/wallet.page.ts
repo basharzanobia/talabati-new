f5b7ff96ba3b28946179aa5fba5b85bce4056353
nihal <108527146+SupportWeb4Z@users.noreply.github.com>
2022-10-12 19:30:54 +0200

    deposit

--- src/app/wallet/wallet.page.ts
+++ src/app/wallet/wallet.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { EwalletServiceProxy } from 'src/shared/service-proxies/service-proxies';
+import { EwalletServiceProxy ,EWallet} from 'src/shared/service-proxies/service-proxies';
 import { AppSessionService } from 'src/shared/session/app-session.service';
 import { AppConsts } from 'src/shared/AppConsts';
+import { AlertController } from '@ionic/angular';  
 
 @Component({
   selector: 'app-wallet',
@@ -9,17 +10,53 @@ import { AppConsts } from 'src/shared/AppConsts';
   styleUrls: ['./wallet.page.scss'],
 })
 export class WalletPage implements OnInit {
-  total_ammount;
-
+  total_amount;
+  amount;
   AppConsts = AppConsts;
-
+ 
   constructor(private _session: AppSessionService,
-    private _ewalletService: EwalletServiceProxy) {
+    private _ewalletService: EwalletServiceProxy,
+    public alertCtrl: AlertController,
+    ) {
 
+  }
+  async  showAlert(msg) {  
+    const alert = await this.alertCtrl.create({  
+      header: 'إضافة رصيد',  
+      subHeader: '',  
+      message:  msg , 
+      buttons: ['تم']  
+    });  
+    await alert.present();  
+    const result = await alert.onDidDismiss();  
+    console.log(result);  
+  }  
+
+  deposit(){
+    const eWallet = new EWallet();
+    eWallet.init({
+      userId:this._session.userId,
+      amount:this.amount,
+      transactionType:1
+    });
+   
+    this._ewalletService.deposite(eWallet).subscribe(
+      (res) => {
+        this.showAlert('تمت إضافة الرصيد بنجاح!' );  
+        this.total_amount+=this.amount;
+       this.amount='';
+        
+      },
+      async (error) => {
+        // Unexpected result!
+        // await this.presentAlert('فشل', 'حدث خطأ حاول مرة أخرى', null);
+        this.showAlert('لم تتم إضافة الرصيد بنجاح, حاول مرة أخرى' ); 
+        console.log('error ', error);
+      });
   }
 
   ngOnInit(): void {
-    this._ewalletService.totalbyuserid(this._session.userId).subscribe((res: number) => this.total_ammount = res);
+    this._ewalletService.totalbyuserid(this._session.userId).subscribe((res: number) => this.total_amount = res);
   }
 
 }
