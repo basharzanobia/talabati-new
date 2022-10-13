cd9fd76e4b175b850c74c71dff7c5223eb563f83
supportWeb4Z <108527146+SupportWeb4Z@users.noreply.github.com>
2022-10-13 12:02:30 +0200

    exit confirmation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
 import { Component } from '@angular/core';
+import { Optional } from '@angular/core';
+import { IonRouterOutlet, Platform, AlertController } from '@ionic/angular';
 
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { Location } from '@angular/common';
+import { App } from '@capacitor/app';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+ 
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private _location: Location,
+    public alertController: AlertController,
+    @Optional() private routerOutlet?: IonRouterOutlet
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+
+
+    this.platform.backButton.subscribeWithPriority(10, (processNextHandler) => {
+      console.log('Back press handler!');
+      if (this._location.isCurrentPathEqualTo('/home')) {
+
+        // Show Exit Alert!
+       
+        this.showExitConfirm();
+        processNextHandler();
+      } else {
+
+        // Navigate to back page
+        console.log('Navigate to back page');
+        this._location.back();
+
+      }
+
+    });
+
+    this.platform.backButton.subscribeWithPriority(5, () => {
+      console.log('Handler called to force close!');
+      this.alertController.getTop().then(r => {
+        if (r) {
+          navigator['app'].exitApp();
+        }
+      }).catch(e => {
+        console.log(e);
+      })
+    });
+
+  }
+
+  showExitConfirm() {
+    this.alertController.create({
+      header: 'الخروح من التطبيق',
+      message: 'هل أنت متأكد من الخروج',
+      backdropDismiss: false,
+      buttons: [{
+        text: 'البقاء',
+        role: 'إلغاء',
+        handler: () => {
+          console.log('Application exit prevented!');
+        }
+      }, {
+        text: 'الخروج',
+        handler: () => {
+          navigator['app'].exitApp();
+        }
+      }]
+    })
+      .then(alert => {
+        alert.present();
+      });
+  }
+
 }
