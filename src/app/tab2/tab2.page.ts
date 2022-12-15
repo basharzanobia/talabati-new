5039be392be3befc04084d2d5003575009d39e0a
Hiba-92 <hiba.hhm.19@gmail.com>
2022-12-15 14:02:39 +0200

    review driver only completed orders

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppOrderStatusType } from 'src/shared/AppEnums';
-import { Order,SubOrder,SuborderapiServiceProxy, OrderapiServiceProxy } from 'src/shared/service-proxies/service-proxies';
+import { Order,UserReview,ReviewuserapiServiceProxy,SubOrder,SuborderapiServiceProxy, OrderapiServiceProxy } from 'src/shared/service-proxies/service-proxies';
 import { AppSessionService } from 'src/shared/session/app-session.service';
 import { AppConsts } from 'src/shared/AppConsts';
 
@@ -13,19 +13,57 @@ export class Tab2Page implements OnInit {
   customFormatter;
   //orders: Order[] = [];
   orders: SubOrder[] = [];
+  driverRating : number[] = [];
 
   AppConsts = AppConsts;
   AppOrderStatusType = AppOrderStatusType;
 
   constructor(private _session: AppSessionService,
     private _subOrderService: SuborderapiServiceProxy,
+    private _reviewUserapiService:ReviewuserapiServiceProxy,
     private _orderService: OrderapiServiceProxy) {
 
   }
 
   ngOnInit(): void {
     //this._orderService.getrequestsbycreatorid(this._session.userId).subscribe((res: Order[]) => this.orders = res);
-    this._subOrderService.getbycreatorid(this._session.userId).subscribe((res: SubOrder[]) => this.orders = res);
+    this._subOrderService.getbycreatorid(this._session.userId).subscribe((res: SubOrder[]) => {
+      this.orders = res;
+      this.orders .forEach(or => {
+        this.getReview(or.userId);
+      });
+    });
   }
+
+  getReview(userId){
+    this._reviewUserapiService.getratingofuser(userId).subscribe(
+      (res) => {    
+        console.log(res);
+        this.driverRating[userId] = res;
+      },
+      async (error) => {
+        console.log('error ', error);
+      });
+  }
+
+  addDriverReview(rate,userId){
+    
+    const review = new UserReview();
+    review.init({
+      userId:userId,
+      raterId:this._session.userId,
+      rating:rate
+    });
+
+    this._reviewUserapiService.create(review).subscribe(
+      (res) => {    
+        this.getReview(userId);
+      },
+      async (error) => {
+        // Unexpected result!
+        // await this.presentAlert('فشل', 'حدث خطأ حاول مرة أخرى', null);
+        console.log('error ', error);
+      });
+}
 
 }
