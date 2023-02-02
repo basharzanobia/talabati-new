56c22aee1f5ae37eef8528b43d3342f20ccb136a
Hiba-92 <hiba.hhm.19@gmail.com>
2023-02-02 11:45:15 +0200

    edit profile

--- src/app/edit-profile/edit-profile.page.ts
+++ src/app/edit-profile/edit-profile.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppAuthService } from 'src/shared/auth/app-auth.service';
+import { AppSessionService } from 'src/shared/session/app-session.service';
+import { LoginResponseModel ,UserapiServiceProxy,ApplicationUser,EditUserAccount} from 'src/shared/service-proxies/service-proxies';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
@@ -9,25 +13,55 @@ export class EditProfilePage implements OnInit {
   selectedImage;
   showPreview;
   usePicker = true;
-  name;
-  PhoneNumber;
-  password;
+  
+  passError="";
 
-  constructor() { }
+  login_id="0";
+  user = new EditUserAccount();
+
+  constructor(private _auth: AppAuthService,
+    private _userService : UserapiServiceProxy,
+    private userSession :AppSessionService,
+    private _router : Router
+    ) {
+
+     }
 
   ngOnInit() {
-  }
-
-  onFileChosen(event) {
-
-  }
-
-  onPickImage() {
-
+    this.login_id = this.userSession.userId;
+    this._userService.getuserbyid(this.login_id).subscribe((res:ApplicationUser)=>
+      {
+        this.user.name =res.name;
+        this.user.phone =res.phoneNumber;
+        this.user.email = res.email;
+        this.user.currentPassword = "";
+        this.user.newPassword = "";
+        this.user.confirmPassword = "";
+      });
   }
 
   save() {
-    
+    if(this.user.currentPassword!=null){
+      if(this.user.newPassword!=null){
+        if(this.user.confirmPassword!=null && this.user.confirmPassword==this.user.newPassword){
+          this._userService.changepassword(this.user).subscribe((res:boolean)=>{
+            this._router.navigate(['/tabs/profile']);
+          });
+        }
+        else{
+          this.passError='كلمة المرور وتأكيد كلمة المرور غير متطابقتان';
+        }
+      }
+      else {
+        this._userService.changepassword(this.user).subscribe((res:boolean)=>{
+          console.log("password not changed");
+        });
+        //this.passError='يرجى إدخال كلمة المرور الجديدة';
+      }
+    }
+    else {
+      this.passError='يرجى إدخال كلمة المرور';
+    }
   }
 
 }
